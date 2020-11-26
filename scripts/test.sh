385778939ee5bf1ee3efc15a68ab74f793455e74
MichaelaCook1 <MCook1@qa.com>
2020-11-26 16:33:56 +0000

    adding test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
-cd frontend/
-pip3 install -r requirements.txt
-python3 -m pytest --cov . 
 
-cd ..
 
-cd backend/
-pip3 install -r requirements.txt
-python3 -m pytest --cov .
+docker build -t testing-img -f testing/Dockerfile .
+docker run -it -d --name testing-cont testing-img
 
+docker exec testing-cont pytest ./frontend --cov ./frontend
+
+docker exec testing-cont pytest ./backend --cov ./backend
+
+docker stop testing-cont 
+docker rm testing-cont
